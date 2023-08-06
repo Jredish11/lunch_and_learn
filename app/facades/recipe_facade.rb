e96e1dc270377500db725018abbdb1fe025389ef
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-06 13:53:17 -0600

    refactor recipes method, fix key names issue where data wasnt being pulled

--- app/facades/recipe_facade.rb
+++ app/facades/recipe_facade.rb
@@ -1,3 +1,21 @@
 class RecipeFacade
-  
+  def initialize(country)
+    @country = country
+  end
+
+  def recipes
+    json = RecipeService.get_recipe(@country)
+    recipe_array = []
+    json.each do |hit|
+      rec_hash = { 
+        uri: hit[:uri],
+        label: hit[:label],
+        country: @country,
+        image: hit[:image]
+      }
+      recipes = Recipe.new(rec_hash)
+      recipe_array << recipes
+    end
+    recipe_array
+  end
 end
