ade3fb2125a30edf6df35cb4f27989b17094e601
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-06 13:55:57 -0600

    add tests for recipe service

--- spec/services/recipe_service_spec.rb
+++ spec/services/recipe_service_spec.rb
@@ -5,19 +5,17 @@ describe RecipeService do
     describe "#get_recipe(country)" do
       it "get recipe by random country no input by user" do
 
-        country = CountryFacade.random_country
+        country = "thailand"
 
         recipes = RecipeService.get_recipe(country)
-        recipes[:hits].each do |hit|
+        recipes.each do |hit|
           expect(hit).to be_a(Hash)
-          expect(hit).to have_key(:recipe)
-          expect(hit[:recipe]).to be_a(Hash)
-          expect(hit[:recipe]).to have_key(:uri)
-          expect(hit[:recipe][:uri]).to be_a(String)
-          expect(hit[:recipe]).to have_key(:label)
-          expect(hit[:recipe][:label]).to be_a(String)
-          expect(hit[:recipe]).to have_key(:image)
-          expect(hit[:recipe][:image]).to be_a(String)
+          expect(hit).to have_key(:uri)
+          expect(hit[:uri]).to be_a(String)
+          expect(hit).to have_key(:label)
+          expect(hit[:label]).to be_a(String)
+          expect(hit).to have_key(:image)
+          expect(hit[:image]).to be_a(String)
         end
       end
     end
