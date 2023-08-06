c4ada861fff8dc6223c857cce863547856ac9532
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-06 13:52:39 -0600

    add tests for recipe poros

--- spec/poros/recipe_spec.rb
+++ spec/poros/recipe_spec.rb
@@ -3,12 +3,17 @@ require "rails_helper"
 RSpec.describe Recipe do
   describe "existance" do
     it "exits and has attributes" do
-      country = 'thailand'
+     
+      country = "mexico"
+      recipe = RecipeFacade.new(country).recipes
 
-      recipes = RecipeService.get_recipe(country)
-      
-
-
+      expect(recipe.first).to be_a(Recipe)
+      expect(recipe.first.id).to eq(nil)
+      expect(recipe.first.type).to eq("recipe")
+      expect(recipe.first.title).to be_a(String)
+      expect(recipe.first.url).to be_a(String)
+      expect(recipe.first.country).to be_a(String)
+      expect(recipe.first.image).to be_a(String)
     end
   end
 end
