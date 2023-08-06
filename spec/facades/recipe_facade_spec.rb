9bcc361b73507b9c46927ef64e4da5dec043a5df
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-06 13:52:24 -0600

    tests for recipefacade recipes method

---
+++ spec/facades/recipe_facade_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe RecipeFacade do
+  describe "instance methods" do
+    describe "#recipes" do
+      it "pulls up list recipe objects based on a country input" do
+        country = "thailand"
+        
+        recipes = RecipeFacade.new(country).recipes
+        recipes.each do |recipe|
+          expect(recipe).to be_a(Recipe)
+          expect(recipe.id).to eq(nil)
+          expect(recipe.type).to eq("recipe")
+          expect(recipe.title).to be_a(String)
+          expect(recipe.url).to be_a(String)
+          expect(recipe.country).to be_a(String)
+          expect(recipe.image).to be_a(String)
+        end
+      end
+    end
+  end
+end
