d7bbca0b5773b00e8f4ca83639c9cbac1f8333ca
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-05 22:43:49 -0600

    gems, routes, yml

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :recipes, only: [:index]
+      resources :countries, only: [:index]
+    end
+  end
 end
