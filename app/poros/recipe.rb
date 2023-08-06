3714b65b0f8d5e589279bdda321560483266bd6b
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-06 13:53:42 -0600

    add id and type to poro

--- app/poros/recipe.rb
+++ app/poros/recipe.rb
@@ -1,11 +1,17 @@
 class Recipe
+  attr_reader :id,
+              :type,
+              :title,
+              :url,
+              :country,
+              :image
 
   def initialize(data)
     @id = nil
-    @type = 'recipe'
-    @title = data[:title]
-    @url = data[:url]
+    @type = "recipe"
+    @title = data[:label]
+    @url = data[:uri]
     @country = data[:country]
-    @image_url = data[:image]
+    @image = data[:image]
   end
 end
