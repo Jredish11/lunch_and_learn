04ce0104569b0193a72d2eb2cb65e34383b9800e
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-06 13:55:21 -0600

    uncomment route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :recipes, only: [:index]
-      resources :countries, only: [:index]
+      # resources :countries, only: [:index]
     end
   end
 end
