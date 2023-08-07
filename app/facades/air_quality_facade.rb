c0dd5705a7c12486b5f6bbaa39642d04d8031ba6
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-07 10:36:42 -0600

    set up facades, add air_quality method

---
+++ app/facades/air_quality_facade.rb
@@ -0,0 +1,17 @@
+class AirQualityFacade
+  # def initialize(country)
+  #   @country = "United States"
+  # end
+
+  def self.air_quality(country)
+    json = AirQualityService.new.get_air_quality(country)
+    
+    aq_hash = {
+      city: country,
+      overall_aqi: json[:overall_aqi],
+      :"PM2.5" => json[:"PM2.5"][:concentration],
+      CO: json[:CO][:concentration]
+    }
+    aq = AirQuality.new(aq_hash)
+  end
+end
