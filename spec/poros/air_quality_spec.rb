e6b754ca9a15f6f7772113e84b99e85fd3d5e156
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-07 10:56:40 -0600

    poros spec for aq

--- spec/poros/air_quality_spec.rb
+++ spec/poros/air_quality_spec.rb
@@ -4,10 +4,17 @@ RSpec.describe AirQuality do
   describe "existance" do
     it "exits and has attributes" do
      
-      country = "mexico"
+      country = "japan"
 
       aq = AirQualityFacade.air_quality(country)
 
+      expect(aq).to be_a(AirQuality)
+      expect(aq.id).to eq(nil)
+      expect(aq.type).to eq("air_quality")
+      expect(aq.aqi).to be_a(Integer)
+      expect(aq.co_concentration).to be_a(Float)
+      expect(aq.country).to be_a(String)
+      expect(aq.pm25_concentration).to be_a(Float) 
     end
   end
 end
