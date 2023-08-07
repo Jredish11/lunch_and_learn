6ce1b352d47b361eecf317a69bdecd5dd8f8e6ca
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-07 10:56:49 -0600

    add id and type

--- app/poros/air_quality.rb
+++ app/poros/air_quality.rb
@@ -2,9 +2,13 @@ class AirQuality
   attr_reader :country, 
               :aqi, 
               :pm25_concentration, 
-              :co_concentration
+              :co_concentration,
+              :type,
+              :id
 
   def initialize(data)
+    @id = nil
+    @type = "air_quality"
     @country = data[:city]
     @aqi = data[:overall_aqi]
     @pm25_concentration = data[:"PM2.5"]
