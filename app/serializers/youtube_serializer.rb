6e27c1cfeb2bd57efccff92b19e0b2d9332c4c2b
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-08 12:10:11 -0600

    add youtubes serializer

---
+++ app/serializers/youtube_serializer.rb
@@ -0,0 +1,18 @@
+class YoutubeSerializer
+  include JSONAPI::Serializer
+  attributes :country
+  
+  attribute :video do |yt|
+    {
+      title: yt.title,
+      youtube_video_id: yt.youtube_video_id,
+    }
+  end
+
+  attribute :images do |yt|
+      {
+        alt_tag: yt.alt_tag,
+        url: yt.url,
+      }
+  end
+end
