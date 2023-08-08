0634fdc333c0850f741aa28bcc84c7bbdd8ab843
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-08 10:47:32 -0600

    add tests

---
+++ spec/poros/youtube_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe Youtube do
+  describe "existance" do
+    it "exits and has attributes" do
+      country = "laos"
+
+      video = YoutubeFacade.new(country).videos.first
+
+      expect(video.country).to be_a(String)
+      expect(video.id).to eq(nil)
+      expect(video.type).to eq("learning_resource")
+      expect(video.title).to be_a(String)
+      expect(video.youtube_video_id).to be_a(String)
+      
+      expect(video.images).to eq(nil)
+    end
+  end
+end
