64187dfe4469ad20bd025bea676ca16388bc0a3c
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-08 12:10:40 -0600

    add index action method to take care of api request

---
+++ app/controllers/api/v1/youtubes_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::YoutubesController < ApplicationController
+  def index
+    country = params[:country]
+    videos = YoutubeFacade.new(country).videos
+    render json: YoutubeSerializer.new(videos)
+  end
+end
