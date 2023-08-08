6b69ca7e7f8533e9d0d3b866bcd3bb5964b8f829
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-08 16:28:11 -0600

    don't know why this isn't creating an array of the youtube objects, pry is showing it build on and throw it into an array but not the whole list, hit api limit

--- app/facades/youtube_facade.rb
+++ app/facades/youtube_facade.rb
@@ -8,36 +8,18 @@ class YoutubeFacade
     response_image = ImageService.get_images(@country)
     vids_array = []
 
-    
-    
-    
-    empty_array = []
-    response[:items].each do |item|
-      response_image[:results].each do |image|
-        if response_image[:results].blank?
-          empty_image_response = {
-            country: @country,
-            video_id: item[:id][:videoId],
-            title: item[:snippet][:title],
-            alt_description: image[:alt_description],
-            urls: image[:urls][:regular]
-            
-          }
-          imgs = Youtube.new(empty_image_response)
-          empty_array << imgs
-          end
-          empty_array 
-        else
-          yt_hash = {
+    require 'pry'; binding.pry
+    response[:items].map do |item|
+    response_image[:results].drop(1).map do |image|
+        yt_hash = {
           country: @country,
           video_id: item[:id][:videoId],
           title: item[:snippet][:title],
           alt_description: image[:alt_description],
           urls: image[:urls][:regular]
-          }
-          vids = Youtube.new(yt_hash)
-          vids_array << vids
-        end
+        }
+        vids = Youtube.new(yt_hash)
+        vids_array << vids
       end
       vids_array
     end
