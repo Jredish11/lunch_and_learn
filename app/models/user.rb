0cddc30029492b15832a2e4d314ae1b530a22b2b
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-08 19:47:38 -0600

    create user model, add validations, bcrypt

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  has_secure_password
+
+  before_create :generate_api_key
+
+  private
+
+  def generate_api_key
+    self.api_key = SecureRandom.hex(16)
+  end
+end
