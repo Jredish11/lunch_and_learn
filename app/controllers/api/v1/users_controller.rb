e9b1a9324de983b103ba2302f28761bce7a44aa6
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-08 21:00:24 -0600

    refactor private method, add serializer to render

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,18 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    user = User.new(user_params)
+    user.generate_api_key
     require 'pry'; binding.pry
+    if user.save 
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
