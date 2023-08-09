f812795daf09261a716710da625dbea02f389afa
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-09 08:14:03 -0600

    add index action

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,11 +1,22 @@
 class Api::V1::FavoritesController < ApplicationController
+
+  def index
+    user = User.find_by(api_key: params[:api_key])
+    if user.nil?
+      render json: { error: "Invalid api_key" }, status: :bad_request 
+    else
+      favs = user.favorites
+      render json: FavoriteSerializer.new(favs)
+    end
+  end
+
   def create
     user = User.find_by(api_key: params[:api_key])
 
     if user.nil?
       render json: { error: "Invalid api_key" }, status: :bad_request
     else
-      favorite = user.favorites.create(favorite_params)
+      user.favorites.create(favorite_params)
       render json: { success: "Favorite added successfully" }, status: :created
     end
   end
